d159224cd83b4a967d6ed73764b9263a0870d197
github username <github email address>
2019-12-01 12:48:53 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
+  get 'static/home', to: 'static#home'
   get 'admin/home', to: 'store_admin#home'
   get 'admin/orders', to: 'store_admin#orders'
   get 'admin/invoice', to: 'store_admin#invoice'
